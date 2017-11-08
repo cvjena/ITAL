94f25562e6a06d12449246dcdef06594b5cfbc66
Björn Barz <bjoern.barz@uni-jena.de>
2017-11-08 16:03:04 +0100

    Fixed image names in learning visualization

--- viz_utils.py
+++ viz_utils.py
@@ -141,14 +141,14 @@ def plot_learning_step(dataset, queries, relevance, learner, ret, fb):
         fig, axes = plt.subplots(6, cols, figsize = (cols, 6))
         for query, ax in zip(queries, axes[0]):
             ax.imshow(canonicalize_image(dataset.imgs_train[query]), interpolation = 'bicubic', cmap = plt.cm.gray)
-            ax.set_xlabel(canonicalize_img_name(dataset.imgs_train[query]))
+            ax.set_title(canonicalize_img_name(dataset.imgs_train[query]), fontsize = 6)
         for r, ax in zip(ret, axes[1]):
             ax.imshow(canonicalize_image(dataset.imgs_train[r]), interpolation = 'bicubic', cmap = plt.cm.gray)
-            ax.set_xlabel(canonicalize_img_name(dataset.imgs_train[r]))
+            ax.set_title(canonicalize_img_name(dataset.imgs_train[r]), fontsize = 6)
         top_ret = np.argsort(learner.rel_mean)[::-1][:cols*(len(axes)-2)]
         for r, ax in zip(top_ret, axes[2:].ravel()):
             ax.imshow(canonicalize_image(dataset.imgs_train[r]), interpolation = 'bicubic', cmap = plt.cm.gray)
-            ax.set_xlabel(canonicalize_img_name(dataset.imgs_train[r]))
+            ax.set_title(canonicalize_img_name(dataset.imgs_train[r]), fontsize = 6)
         for ax in axes.ravel():
             ax.axis('off')
         fig.tight_layout()
